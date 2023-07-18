f4ac38e098479a3fe9bf4203499e32fca103a808
Usama Shahid <uxamashahid364@gmail.com>
2023-07-18 20:07:46 +0500

    new file:   .env 	new file:   components/Avatar.tsx 	new file:   components/Button.tsx 	new file:   components/Form.tsx 	new file:   components/Header.tsx 	new file:   components/ImageUpload.tsx 	new file:   components/Input.tsx 	new file:   components/Layout.tsx 	new file:   components/Modal.tsx 	new file:   components/Modals/EditModal.tsx 	new file:   components/Modals/LoginModal.tsx 	new file:   components/Modals/RegisterModal.tsx 	new file:   components/NotificationsFeed.tsx 	new file:   components/layout/FollowBar.tsx 	new file:   components/layout/Sidebar.tsx 	new file:   components/layout/SidebarItem.tsx 	new file:   components/layout/SidebarLogo.tsx 	new file:   components/layout/SidebarTweetButton.tsx 	new file:   components/posts/CommentFeed.tsx 	new file:   components/posts/CommentItem.tsx 	new file:   components/posts/PostFeed.tsx 	new file:   components/posts/PostItem.tsx 	new file:   components/users/UserBio.tsx 	new file:   components/users/UserHero.tsx 	new file:   hooks/useCurrentUser.ts 	new file:   hooks/useEditModal.ts 	new file:   hooks/useFollow.ts 	new file:   hooks/useLike.ts 	new file:   hooks/useLoginModal.ts 	new file:   hooks/useNotifications.ts 	new file:   hooks/usePost.ts 	new file:   hooks/usePosts.ts 	new file:   hooks/useRegisterModal.ts 	new file:   hooks/useUser.ts 	new file:   hooks/useUsers.ts 	new file:   libs/fetcher.ts 	new file:   libs/prismadb.ts 	new file:   libs/serverAuth.ts 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/_app.tsx 	deleted:    pages/_document.tsx 	new file:   pages/api/auth/[...nextauth].ts 	new file:   pages/api/comments.ts 	new file:   pages/api/current.ts 	new file:   pages/api/edit.ts 	new file:   pages/api/follow.ts 	deleted:    pages/api/hello.ts 	new file:   pages/api/like.ts 	new file:   pages/api/notifications/[userId].ts 	new file:   pages/api/posts/[postId].ts 	new file:   pages/api/posts/index.ts 	new file:   pages/api/register.ts 	new file:   pages/api/users/[userId].ts 	new file:   pages/api/users/index.ts 	modified:   pages/index.tsx 	new file:   pages/notifications.tsx 	new file:   pages/posts/[postId].tsx 	new file:   pages/users/[userId].tsx 	new file:   prisma/schema.prisma 	new file:   public/images/placeholder.png 	modified:   styles/globals.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
+import Layout from '@/components/Layout'
+import EditModal from '@/components/Modals/EditModal'
+import LoginModal from '@/components/Modals/LoginModal'
+import RegisterModal from '@/components/Modals/RegisterModal'
 import '@/styles/globals.css'
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import { Toaster } from 'react-hot-toast'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Toaster />
+      <EditModal />
+      <RegisterModal />
+      <LoginModal />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  )
 }
